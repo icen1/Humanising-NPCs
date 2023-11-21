295466e6a40d4a807ce2aeee3eba1aa7dab55ba4
icen1 <kokeamr@hotmail.com>
2023-11-21 02:43:10 +0000

    Made the UI more tolerable by using ttkbootstrap. Implemented a scrollable frame, and better submit button

--- humanising_npcs/main.py
+++ humanising_npcs/main.py
@@ -1,9 +1,10 @@
 # make a GUI to take input for the state machine and then display image produced of the machine
 # it should take the input as a number between 1 and -1 as a slider
-import itertools
-import time
-from tkinter import Button, Label, Scale, Tk
-from PIL import ImageTk, Image
+
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from ttkbootstrap.scrolled import ScrolledFrame
 from random import uniform as randomuniform
 
 from traits import Traits
@@ -12,7 +13,6 @@ from traits import Traits
 # add a few examples on how a character with each combinations of traits would behave
 # Generate examples for all combinations of traits
 def generate_examples(combinations):
-    dict_of_examples = {}
     for i in range(1, len(combinations) + 1):
         examples = []
         if "Lazy" in combinations:
@@ -36,31 +36,36 @@ def generate_examples(combinations):
 
 def main():
     # create a window
-    window = Tk()
-    # set the title
-    window.title("Humanising NPCs")
-    # set the window size
-    window.geometry("1600x900")
-    # set the window background color
-    window.config(background="white")
+    window = ttk.window.Window(title="Humanising NPCs",themename="lumen")
+    
+    sf = ScrolledFrame(window, autohide=True, width=1080)
+    sf.pack(fill=BOTH, expand=YES, padx=10, pady=10)
+    
+
+    # Create style object
+    style = ttk.style.Style(theme='lumen')  # You can choose a different theme
+
+    # Apply the style to the window window
+    style.configure(style,window)
+    style.configure(style,sf)
+
+
     # read the slider value
     slider_value = -2
     # update the slider value
     def update_slider_value(value):
         nonlocal slider_value
         slider_value = float(value)
-    # add a slider to the window between 1 and -1
-    window.slider = Scale(window, from_=1, to=-1, orient="horizontal", label="Goodness of NPC", length=600, tickinterval=0.1, resolution=0.1, showvalue=True, command=update_slider_value)
-    # add the slider to the window
-    window.slider.pack()
+        print(slider_value)
+    # Create slider
+    slider = ttk.Scale(sf,from_=-1, to=1, value=0,command=update_slider_value)
+    slider.pack()
     counter = 0
     # add a submit button to the window to update the slider value
     def submit():
         nonlocal counter
         counter += 1 
         sm = Traits()
-        img_path = "./readme_trafficlightmachine_" + time.strftime("%Y%m%d-%H%M%S") + ".png"
-        sm._graph().write_png(img_path)
         chosen_traits = []
         mode = -1 if slider_value > 0 else 1
         if slider_value+(mode*randomuniform(0,1)) < 0:
@@ -97,18 +102,16 @@ def main():
             sm.send('brave_to_end')
         # on submit, display the list of traits
         examples = generate_examples(chosen_traits)
-        window.traits = Label(window, text=f"Traits of NPC {counter}: " + ", ".join(chosen_traits))
-        # print the examples
-        window.examples = Label(window, text="----Examples: " + ", ".join(examples))
-        window.traits.pack()
-        window.examples.pack()
-    window.submit = Button(window, text="Submit", command=submit)
-    window.submit.pack()
-    # # display the image
-    # img = ImageTk.PhotoImage(Image.open("./readme_trafficlightmachine_20231102-124203.png"))
-    # panel = Label(window, image=img)
-    # panel.pack(side="bottom", fill="both", expand="yes")
-    # display the window
+        # Create labels
+        label1 = ttk.Label(sf, text=f"Traits of NPC {counter}: " + ", ".join(chosen_traits), padding="5")
+        label2 = ttk.Label(sf, text="----Examples: " + ", ".join(examples), padding="5")
+        label1.pack()
+        label2.pack()
+        style.configure(style,label1)
+        style.configure(style,label2)
+    # Create button
+    button = ttk.Button(sf, text="Submit", command=submit, bootstyle=OUTLINE)
+    button.pack()
     window.mainloop()
     
 
@@ -117,4 +120,43 @@ def main():
 if __name__ == "__main__":
     main()
     
-    
+
+""" 
+# Create style object
+style = Style(theme='lumen')  # You can choose a different theme
+
+# Apply the style to the window window
+style.configure(window)
+
+# Create and configure frames
+frame = ttk.Frame(window, padding="10")
+frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+style.configure(frame)
+
+# Create labels
+label1 = ttk.Label(frame, text=f"Traits of NPC {counter}: " + ", ".join(chosen_traits), padding="5")
+label1.grid(row=0, column=0, sticky=tk.W)
+style.configure(label1)
+
+label2 = ttk.Label(frame, text="----Examples: " + ", ".join(examples), padding="5")
+label2.grid(row=1, column=0, sticky=tk.W)
+style.configure(label2)
+
+label_value = ttk.Label(frame, text="Slider Value: 0", padding="5")
+label_value.grid(row=2, column=0, columnspan=2, sticky=tk.W)
+style.configure(label_value)
+
+label_result = ttk.Label(frame, text="", padding="5")
+label_result.grid(row=3, column=0, columnspan=2, sticky=tk.W)
+style.configure(label_result)
+
+# Create slider
+slider = ttk.Scale(frame, from_=0, to=100, orient=tk.HORIZONTAL, showvalue=True, command=update_slider_value)
+slider.grid(row=2, column=1, sticky=(tk.W, tk.E))
+style.configure(slider)
+
+# Create button
+button = ttk.Button(frame, text="Submit", command=osubmit)
+button.grid(row=3, column=1, sticky=(tk.W, tk.E))
+style.configure(button)
+"""
